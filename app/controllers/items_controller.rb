a61cc56f4ec45a071b7f84d2dd6e1495c9c15111
JadFu <84548552+JadFu@users.noreply.github.com>
2023-07-20 05:20:34 -0500

    fixing image error on item show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,7 @@ class ItemsController < ApplicationController
     def index
       @items = Item.all.page(params[:page]).per(10) 
     end
+    def show
+        @item = Item.find(params[:id])
+    end
   end
