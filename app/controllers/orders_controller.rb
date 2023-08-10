7bacd409419b5ddb83db12d43f5c628d47486a88
JadFu <84548552+JadFu@users.noreply.github.com>
2023-08-10 14:06:55 -0500

    add tax informatoin to user order record

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,16 @@ class OrdersController < ApplicationController
     def show
         @order = Order.find_by(id: params[:id])
         @order_items = OrderItem.where(order_id: params[:id])
+        @user = User.find_by(id: @order.user_id)
+        @tax = Tax.find_by(id: @user.tax_id)
+        @tax_rate = total_tax
+    end
+
+    private
+
+    def total_tax
+        total_tax_rate = @tax.GST + @tax.PST + @tax.HST
+        total_tax_rate
     end
 end
   
